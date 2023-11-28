5cb14216d14cbf098968de19c529f26ea7d474bf
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2023-11-28 13:46:05 +0200

    Added handlings errors and Not-found pages

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,6 +9,7 @@ import {
   User,
   Revenue,
 } from './definitions';
+
 import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
@@ -163,7 +164,8 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
